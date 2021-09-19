5f3752395c9484c667922f8922f8db930502744e
Lubomír Rýgl <lubomir.rygl@aevi.com>
2021-09-19 23:19:57 +0200

    homeCommit

--- src/main/java/com/application/ecommerce/Model/Product.java
+++ src/main/java/com/application/ecommerce/Model/Product.java
@@ -1,7 +1,19 @@
 package com.application.ecommerce.Model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Product")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String productName;
+
+
+
+
+
 
 }
