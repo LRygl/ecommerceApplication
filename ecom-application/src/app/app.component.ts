7cc91b3f237e6171b58580f9d66ca88b758c4ecc
Lubomír Rýgl <lubomir.rygl@aevi.com>
2021-09-24 19:48:51 +0200

    working on angular implementation

--- ecom-application/src/app/app.component.ts
+++ ecom-application/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Manufacturer } from './manufacturer/Manufacturer.ts';
+import { ManufacturerServiceService } from './services/manufacturer-service.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ecom-application';
+  manufacturers = Manufacturer[];
+
+  constructor(private router:Router, private manufacturerService: ManufacturerServiceService){}
+
+  getManufacturers(){
+    this.manufacturerService.getManufacturers().subscribe(data => {this.manufacturers = data})
+  }
 }
--- ecom-application/src/app/app.module.ts
+++ ecom-application/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddManufacturerComponent } from './manufacturer/add-manufacturer/add-manufacturer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddManufacturerComponent
   ],
   imports: [
     BrowserModule,
